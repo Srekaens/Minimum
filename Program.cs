0eb4d03f613fef29fa7e880e1470facf6c236c7a
Justin Didelot <163373490+Srekaens@users.noreply.github.com>
2024-08-22 11:52:04 +0200

    Ajout du program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿static int Min(int a, int b)
+{
+    if (a < b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+Console.WriteLine(Min(0, 10));
+Console.WriteLine(Min(0, -10));
